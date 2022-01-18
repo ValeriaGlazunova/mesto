9d03360df346d716ef0440e6ac6a5a5df1b388df
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-01-18 21:57:51 +0500

    добавить JS открыть закрыть попап

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,14 @@ const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const popupSaveButton = document.querySelector('.popup__save-button');
 
-profileEditOpenPopupButton.addEventListener('click', function(){
-    console.log('header button clicked');
-})
+
+function openPopup () {
+    popup.classList.add('popup_opened');
+}
+
+function closePopup () {
+    popup.classList.remove('popup_opened');
+}
+
+profileEditOpenPopupButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
