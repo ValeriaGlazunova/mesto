d3941ee41a01865206e981a279dfdf35e14a004a
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-01-18 21:39:45 +0500

    добавить попап опенед

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const profileEditOpenPopupButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close-button');
+const popupSaveButton = document.querySelector('.popup__save-button');
+
+profileEditOpenPopupButton.addEventListener('click', function(){
+    console.log('header button clicked');
+})
