0ced5c9a1688a7e1cf6bda530a6e966f06bda4ca
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-01-24 20:42:48 +0500

    JS попап коррект структура, добавлены данные в форму из профила

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,38 @@
 const profileEditOpenPopupButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
-const popupSaveButton = document.querySelector('.popup__save-button');
 
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_description');
+
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
 
 function openPopup () {
     popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileDescription.textContent;
 }
 
 function closePopup () {
     popup.classList.remove('popup_opened');
 }
 
-profileEditOpenPopupButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
-popup.addEventListener('click', function(event) {
-    if(event.target === event.currentTarget) {
-        closePopup();
-    }
-});
-
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__name-field');
-let jobInput = document.querySelector('.popup__description-field');
-
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let userName = nameInput.value; 
-    let userDescription = jobInput.value; 
-    let profileName = document.querySelector('.profile__name');
-    let profileDescription = document.querySelector('.profile__description');
-    profileName.textContent = userName;
-    profileDescription.textContent = userDescription;
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
     closePopup ();
 }
 
-    formElement.addEventListener('submit', formSubmitHandler); 
+profileEditOpenPopupButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+// ниже добавлен код для закрытия popup при нажатии мимо поля popup
+//popup.addEventListener('click', function(event) {
+    //if(event.target === event.currentTarget) {
+        //closePopup();
+   // }
+//});
+
+formElement.addEventListener('submit', formSubmitHandler); 
