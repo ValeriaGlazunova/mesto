1a784d749e56033a61221d47facfc063d546aa26
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-02-12 15:32:01 +0500

    добавить валидацию первой формы

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,28 +1,70 @@
-const formElement = document.querySelector(".popup__form_type_edit-profile");
-const formInput = formElement.querySelector(".popup__input");
-const formError = formElement.querySelector(`.${formInput.id}-error`); 
+//функция отправки валидной формы
+const sumbitValidForm = (evt) => {
+  evt.preventDefault();
+}
+
+//функция отображения стиля неверного инпута
+const showInputError = (formError, input) => {
+   formError.textContent = input.validationMessage;
+   input.classList.add('popup__input_type_error');
+};
+
+//функция скрытия стиля неверного инпута
+ const hideInputError = (formError, input) => {
+    formError.textContent = '';
+    input.classList.remove('popup__input_type_error')
+};
+
+//функция отображения неактивной кнопки сохранения
+const disableButton = (button) => {
+  button.setAttribute('disabled', '');
+  button.classList.add('popup__save-button_disabled');
+};
+
+//функция отображения активной кнопки сохранения
+const enableButton = (button) => {
+  button.removeAttribute('disabled');
+  button.classList.remove('popup__save-button_disabled');
+};
+
+//функция проверки корректности введенных данных в инпут
+const isInputValid = (form, input) => {
+  const formError = form.querySelector(`#${input.id}-error`);
+  
+  if(input.validity.valid) {
+    hideInputError(formError, input);
+  } else {
+    showInputError(formError, input);
+  }
+}
+
+//функция проверки состояния кнопки по валидности формы
+const isButtonValid = (form, button) => {
+  if (form.checkValidity()) {
+    enableButton(button);
+  } else {
+    disableButton(button);
+  }
+}
+
+//функция проверки валидности формы
+function enableValidation () {
+  const form = document.querySelector(".popup__form_type_edit-profile");
+    form.addEventListener('submit', sumbitValidForm);
+
+  const formInputs = form.querySelectorAll('.popup__input');
+  const button = form.querySelector('.popup__save-button');
 
 
-const showInputError = (element) => {
-    element.classList.add('popup__input_type_error');
-    formError.classList.add('popup__input-error_active');
-  };
+  formInputs.forEach(input => {
+    input.addEventListener('input', () => {
+      isInputValid(form, input);
+      isButtonValid(form, button);
+    });
+  })
+  
+};
 
-const hideInputError = (element) => {
-    element.classList.remove('popup__input_type_error');
-    formError.classList.remove('popup__input-error_active');
-  };
 
-const isValid = () => {
-    if (!formInput.validity.valid) {
-      showInputError(formInput);
-    } else {
-      hideInputError(formInput);
-    }
-  };
 
-formElement.addEventListener('submit', function (evt) {
-    evt.preventDefault();
-  });
-
-formInput.addEventListener('input', isValid);
+enableValidation ();
