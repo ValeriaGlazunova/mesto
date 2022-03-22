773e353955834255d67ab9dfd5d74af576b29a61
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-03-22 21:46:04 +0500

    добавить класс sectioт

---
+++ scripts/Popup.js
@@ -0,0 +1,17 @@
+class Popup {
+    constructor(popupSelector) {
+        this._popup = document.querySelector(popupSelector);
+    }
+    open() {
+
+    }
+    close() {
+
+    }
+    _handleEscClose() {
+
+    }
+    setEventListeners() {
+        
+    }
+}
