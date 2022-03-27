d5591564c31b4320991281f1f40a3f482d9ad832
Valeria Glazunova <valeriaglazunova@gmail.com>
2022-03-27 18:34:47 +0500

    добавить распределение файлов по директориям

---
+++ utils/constants.js
@@ -0,0 +1,17 @@
+export const profileEditOpenPopupButton = document.querySelector(".profile__edit-button");
+export const profileAddButton = document.querySelector(".profile__add-button");
+
+export const formEditElement = document.querySelector(".popup__form_type_edit-profile");
+export const formAddElement = document.querySelector(".popup__form_type_add-card");
+export const nameInput = document.querySelector(".popup__input_type_name");
+export const jobInput = document.querySelector(".popup__input_type_description");
+
+
+export const validationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__save-button",
+  inactiveButtonClass: "popup__save-button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__input-error_visible",
+};
